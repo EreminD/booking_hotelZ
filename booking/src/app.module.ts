a285b35c1e3713434decc16ad1df9524ece7d3ee
Дмитрий Еремин <mitya1705@gmail.com>
2022-08-27 09:56:33 +0300

    only hotel

--- booking/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BkngModule } from './bkng/bkng.module';
-
-@Module({
-  imports: [BkngModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
--- booking/src/bkng/dto/create-bkng.dto.ts
+++
@@ -1,3 +0,0 @@
-export class CreateBkngDto {
-    roomId: number;
-}
